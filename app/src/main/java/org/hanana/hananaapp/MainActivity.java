109ffbb6097ec1c26c2a18758d5da059fa80cd7d
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2018-05-07 13:34:13 +0530

    minor change

--- app/src/main/java/org/hanana/hananaapp/MainActivity.java
+++ app/src/main/java/org/hanana/hananaapp/MainActivity.java
@@ -11,16 +11,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-<<<<<<< HEAD
-    public void hello(){
-        // hello
-=======
-    private void hello(){
-
->>>>>>> 6dce887360d7aed6acdf8723f27d50e6b2bc98aaaqw
-
-             private void abc(){
-
-            }
-    }
 }
