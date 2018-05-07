9715cc2637de607cb8960812ea51ec9c8ba11200
jananisamaranayake <jananisamaranayake@gmail.com>
2018-05-07 16:50:23 +0530

    janani1

--- app/src/main/java/org/hanana/hananaapp/MainActivity.java
+++ app/src/main/java/org/hanana/hananaapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hello guys1
+        //hello guys12
     }
 }
