9e052f79938909927270d31fb99cce764276206a
gunathilakanethu <gunathilaka.nethu@gmail.com>
2018-05-07 16:28:17 +0530

    just some commit

--- app/src/main/java/org/hanana/hananaapp/MainActivity.java
+++ app/src/main/java/org/hanana/hananaapp/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 =======
     private void hello(){
 
->>>>>>> 6dce887360d7aed6acdf8723f27d50e6b2bc9
+>>>>>>> 6dce887360d7aed6acdf8723f27d50e6b2bc
 
              private void abc(){
 
