7465198786021033d78728e79f86d3acc1361168
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2018-05-07 12:50:24 +0530

    Test commit II

--- app/src/main/java/org/hanana/hananaapp/MainActivity.java
+++ app/src/main/java/org/hanana/hananaapp/MainActivity.java
@@ -18,5 +18,9 @@ public class MainActivity extends AppCompatActivity {
     private void hello(){
 
 >>>>>>> 6dce887360d7aed6acdf8723f27d50e6b2bc98aaaqw
+
+             private void abc(){
+
+            }
     }
 }
