f072aa1576ef1994665adf91cb5ca6b68817d15b
jananisamaranayake <jananisamaranayake@gmail.com>
2018-05-17 13:32:05 +0530

    SignIn

--- app/src/main/java/org/hanana/hananaapp/MainActivity.java
+++ app/src/main/java/org/hanana/hananaapp/MainActivity.java
@@ -1,5 +1,6 @@
 package org.hanana.hananaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,15 +19,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onButtonClick(View v)
-    {
-        if(v.getId()==R.id.bsignIn)
 
-            EditText a = (EditText)findViewById(R.id.Lusername);
-
-            Intent i = new Intent(MainActivity.this, EventsActivity.class);
-            i.putExtra(i)
-            startActivity(i);
-
-    }
 }
--- app/src/main/java/org/hanana/hananaapp/SignUpActivity.java
+++ app/src/main/java/org/hanana/hananaapp/SignUpActivity.java
@@ -1,7 +1,10 @@
 package org.hanana.hananaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void onButtonClick(View v)
+    {
+        if(v.getId()==R.id.bsignIn)
+
+            EditText a = (EditText)findViewById(R.id.lusername);
+
+        Intent i = new Intent(MainActivity.this, EventsActivity.class);
+        i.putExtra(i)
+        startActivity(i);
+
     }
 }
