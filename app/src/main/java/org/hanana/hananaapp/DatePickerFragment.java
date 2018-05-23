b60a3498bbcf1c1cb6aa30af6ce880fea70759ef
Devni Perera <devniperera14@gmail.com>
2018-05-23 15:57:50 +0530

    Add DatePickerFragment class

--- app/src/main/java/org/hanana/hananaapp/DatePickerFragment.java
+++ app/src/main/java/org/hanana/hananaapp/DatePickerFragment.java
@@ -4,28 +4,42 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
 import android.widget.DatePicker;
+
+import org.hanana.hananaapp.models.TimeStamper;
+import org.hanana.hananaapp.models.ViewUpdater;
 
 import java.util.Calendar;
 
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    TimeStamper timeStamper;
+    ViewUpdater viewUpdater;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-//        DatePickerDialog dateSettings = new DatePickerDialog(getActivity());
-
         final Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        DatePickerDialog dialog;
-        dialog = new android.app.DatePickerDialog(getActivity(), this, year, month, day);
-//
+        DatePickerDialog dialog = new DatePickerDialog(getActivity(), this, year, month, day);
+
         return dialog;
     }
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int day) {
-
+        timeStamper.setDate(day, month,year);
+        viewUpdater.updateView();
     }
+
+    public void setTimeStamper(TimeStamper stamper){
+        timeStamper = stamper;
+    }
+
+    public void setViewUpdater(ViewUpdater updater){
+        viewUpdater = updater;
+    }
+
 }
--- app/src/main/java/org/hanana/hananaapp/EventsFragment.java
+++ app/src/main/java/org/hanana/hananaapp/EventsFragment.java
@@ -3,4 +3,5 @@ package org.hanana.hananaapp;
 import android.support.v4.app.Fragment;
 
 public class EventsFragment extends Fragment {
+
 }
