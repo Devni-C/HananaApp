87aaf46e4f471545e32f2f30153f27db3df706ea
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2018-05-28 18:14:37 +0530

    Minor change

--- app/src/main/java/org/hanana/hananaapp/Database/HananaDatabaseSQlite.java
+++ app/src/main/java/org/hanana/hananaapp/Database/HananaDatabaseSQlite.java
@@ -1,28 +1,23 @@
 package org.hanana.hananaapp.Database;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
-import org.hanana.hananaapp.SignIn1;
-
-import sun.awt.SunHints;
 
 
 public class HananaDatabaseSQlite extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "HananaDB";
-    private static final String TABLE_NAME = "SignIn_Table";
-    private static final String COLOUMN_ID = "SignIn_ID";
-    private static final String COLOUMN_USERNAME = "SignIn_Username";
-    private static final String COLOUMN_PASSWORD = "SignIn_Password";
+    private static final String USER = "user";
+    private static final String ID = "id";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
     SQLiteDatabase sqLiteDatabase;
 
-    private static final String TABLE_CREATE = "create table SignIn_Table (SignIn_ID integer primary key not null  auto_increment  ," +
-            "SignIn_Username text not null , SignIn_Password text not null);";
+    private static final String USER_TABLE = "create table "
+            + USER + " (" + ID  + " integer primary key autoincrement not null,"
+             + USERNAME +" text not null ," + PASSWORD + " text not null);";
 
     public HananaDatabaseSQlite(Context context){
         super(context, DATABASE_NAME , null , DATABASE_VERSION);
@@ -30,7 +25,7 @@ public class HananaDatabaseSQlite extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(TABLE_CREATE);
+        sqLiteDatabase.execSQL(USER_TABLE);
         this.sqLiteDatabase = sqLiteDatabase;
     }
 
@@ -58,7 +53,7 @@ public class HananaDatabaseSQlite extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        String query = "DROP TABLE IF EXISTS "+ TABLE_NAME;
+        String query = "DROP TABLE IF EXISTS "+ USER;
         sqLiteDatabase.execSQL(query);
         this.onCreate(sqLiteDatabase);
     }
