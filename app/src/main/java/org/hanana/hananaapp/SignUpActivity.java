914b979d7d87175a7f0c0ea7caea5ebc97a64a73
jananisamaranayake <jananisamaranayake@gmail.com>
2018-05-28 12:14:47 +0530

    SignIn JavaClass 12.14p.m

--- app/src/main/java/org/hanana/hananaapp/SignUpActivity.java
+++ app/src/main/java/org/hanana/hananaapp/SignUpActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
