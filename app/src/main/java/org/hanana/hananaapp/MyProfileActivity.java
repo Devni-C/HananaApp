65596f2a1f4888c8b9d7416da677317cdc557262
shavindumanawadu <snmanawadu@gmail.com>
2018-05-30 13:31:15 +0530

    onceAgain Edit my profile

--- app/src/main/java/org/hanana/hananaapp/MyProfileActivity.java
+++ app/src/main/java/org/hanana/hananaapp/MyProfileActivity.java
@@ -10,6 +10,9 @@ public class MyProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+
+
     }
 
     public void AccSettingsOnClick(View view){
